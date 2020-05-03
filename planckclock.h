0c42b956af0b6341a718faffbc40db2a8c5c44d5
Antony Kellermann <aokellermann@gmail.com>
2020-05-02 23:39:01 -0400

    Changed type from size_t to unsigned long so that header is compatible with C++

--- planckclock.h
+++ planckclock.h
@@ -31,7 +31,7 @@ planck_time_t planck_time(planck_time_t*);
 
 planck_tm* planck_localtime(const planck_time_t*);
 
-size_t planck_strftime(char *s, size_t max, const char *format,
-                       const planck_tm *tm);
+unsigned long planck_strftime(char *s, unsigned long max, const char *format,
+                              const planck_tm *tm);
 
 #endif // PLANCKCLOCK_LIBRARY_H_
